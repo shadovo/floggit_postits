645269d30d3806eb024a9af221e4e52a59ed7e9c
Oscar Rundh <mr.rundh@gmail.com>
2014-08-25 11:52:12 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name floggitPostitsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the floggitPostitsApp
+ */
+angular.module('floggitPostitsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
