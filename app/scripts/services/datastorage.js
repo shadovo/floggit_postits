e127105ed96394afe53faa9d7bc626ce3f71dd4f
Oscar Rundh <mr.rundh@gmail.com>
2014-08-26 13:38:48 +0200

    Adds methods to datastorage to get Postits, Categories and get Categories with Postits attached.

--- app/scripts/services/datastorage.js
+++ app/scripts/services/datastorage.js
@@ -13,21 +13,29 @@ angular.module('floggitPostitsApp')
     // var baseUrl = 'http://api.beta2.se/fp-';
     var baseUrl = 'http://localhost:14782/fp-';
 
-    function basicGet(whiteboard, type) {
+
+    function basicRequest(method, whiteboard, type, data) {
       var deferred = $q.defer();
       var url = baseUrl + whiteboard + '-' + type;
-      $http.get(url).success(function (data) {
+      var requestParams = {};
+      requestParams.method = method;
+      requestParams.url = url;
+      if (data !== undefined) {
+        requestParams.data = data;
+      }
+      $http(requestParams).success(function (data) {
         deferred.resolve(data);
       });
+      return deferred.promise;
     }
 
-  // function basicPost(whiteboard, type, data) {
-  //   var deferred = $q.defer();
-  //   var url = baseUrl + whiteboard + '-' + type;
-  //   $http.post(url, data).success(function (data) {
-  //     deferred.resolve(data);
-  //   });
-  // }
+    function basicGet(whiteboard, type) {
+      return basicRequest('GET', whiteboard, type);
+    }
+
+    function basicPost(whiteboard, type, data) {
+      return basicRequest('POST', whiteboard, type, data);
+    }
 
     function getAllCategoriesFor(whiteboard) {
       return basicGet(whiteboard, 'categories');
@@ -37,63 +45,95 @@ angular.module('floggitPostitsApp')
       return basicGet(whiteboard, 'postits');
     }
 
+    function addPostitToCorrespondingCategory(categories, postit) {
+      var j;
+      for (j = 0; j < categories.length; j = j + 1) {
+        if (postit.category === categories[j].id) {
+          if (categories[j].postits === undefined) {
+            categories[j].postits = [];
+          }
+          categories[j].postits.push(postit);
+        }
+      }
+    }
 
-    // function postDummyData() {
-    //   for (var i = 0; i < dummyData.categories.length; i = i + 1) {
-    //     basicPost('testwhiteboard2', 'categories', dummyData.categories[i]);
-    //   }
-    //   for (var j = 0; j < dummyData.postits.length; j = j + 1) {
-    //     basicPost('testwhiteboard2', 'postits', dummyData.postits[i]);
-    //   }
-    // }
-    // var dummyDataCategories = [{
-    //   'name': 'Todo'
-    // }, {
-    //   'name': 'In progress'
-    // }, {
-    //   'name': 'Done'
-    // }];
+    function getAll(whiteboard) {
+      var deferred = $q.defer();
+      var categoryPromise = getAllCategoriesFor(whiteboard);
+      var postitsPromise, i;
+      categoryPromise.then(function (categories) {
+        postitsPromise = getAllPostitsFor(whiteboard);
+        postitsPromise.then(function (postits) {
+          for (i = 0; i < postits.length; i = i + 1) {
+            addPostitToCorrespondingCategory(categories, postits[i]);
+          }
+        });
+        deferred.resolve(categories);
+      });
+      return deferred.promise;
+    }
 
-    // var dummyDataPostits = [{
-    //   'title': 'Some good deed.',
-    //   'description': 'I am ver good deed that must be done!',
-    //   'color': 'red',
-    //   'category': 1
-    // }, {
-    //   'title': 'Some bad deed.',
-    //   'description': 'I am ver bad deed that must be done!',
-    //   'color': 'blue',
-    //   'category': 1
-    // }, {
-    //   'title': 'Get angry over shit .',
-    //   'description': 'I very important stuff!',
-    //   'color': 'red',
-    //   'category': 1
-    // }, {
-    //   'title': 'Under progress',
-    //   'description': 'I huiehriueh must be done!',
-    //   'color': 'green',
-    //   'category': 2
-    // }, {
-    //   'title': 'Almost done.',
-    //   'description': 'I am ver bad deed that must be done!',
-    //   'color': 'yellow',
-    //   'category': 2
-    // }, {
-    //   'title': 'Done!!',
-    //   'description': 'I huiehriueh must be done be done!',
-    //   'color': 'green',
-    //   'category': 3
-    // }, {
-    //   'title': 'Done Again',
-    //   'description': 'I anojoijojioj jjj must be done!',
-    //   'color': 'blue',
-    //   'category': 3
-    // }];
 
-    // var dummyData = {};
-    // dummyData.categories = dummyDataCategories;
-    // dummyData.postits = dummyDataPostits;
+
+    function postDummyData() {
+      for (var i = 0; i < dummyData.categories.length; i = i + 1) {
+        basicPost('testwhiteboard', 'categories', dummyData.categories[i]);
+      }
+      for (var j = 0; j < dummyData.postits.length; j = j + 1) {
+        basicPost('testwhiteboard', 'postits', dummyData.postits[j]);
+      }
+    }
+    var dummyDataCategories = [{
+      'name': 'Todo',
+      'id': 1
+    }, {
+      'name': 'In progress',
+      'id': 2
+    }, {
+      'name': 'Done',
+      'id': 3
+    }];
+
+    var dummyDataPostits = [{
+      'title': 'Some good deed.',
+      'description': 'I am ver good deed that must be done!',
+      'color': 'red',
+      'category': 1
+    }, {
+      'title': 'Some bad deed.',
+      'description': 'I am ver bad deed that must be done!',
+      'color': 'blue',
+      'category': 1
+    }, {
+      'title': 'Get angry over shit .',
+      'description': 'I very important stuff!',
+      'color': 'red',
+      'category': 1
+    }, {
+      'title': 'Under progress',
+      'description': 'I huiehriueh must be done!',
+      'color': 'green',
+      'category': 2
+    }, {
+      'title': 'Almost done.',
+      'description': 'I am ver bad deed that must be done!',
+      'color': 'yellow',
+      'category': 2
+    }, {
+      'title': 'Done!!',
+      'description': 'I huiehriueh must be done be done!',
+      'color': 'green',
+      'category': 3
+    }, {
+      'title': 'Done Again',
+      'description': 'I anojoijojioj jjj must be done!',
+      'color': 'blue',
+      'category': 3
+    }];
+
+    var dummyData = {};
+    dummyData.categories = dummyDataCategories;
+    dummyData.postits = dummyDataPostits;
     // postDummyData();
 
     return {
@@ -101,7 +141,8 @@ angular.module('floggitPostitsApp')
         return baseUrl;
       },
       getAllCategoriesFor: getAllCategoriesFor,
-      getAllPostitsFor: getAllPostitsFor
+      getAllPostitsFor: getAllPostitsFor,
+      getAll: getAll
     };
   });
 
