24ac6d08d74bbbe14cc66312470c55ed4297ea4c
Oscar Rundh <mr.rundh@gmail.com>
2014-08-26 10:03:50 +0200

    Possible to add dummy data to db

--- app/scripts/services/datastorage.js
+++ app/scripts/services/datastorage.js
@@ -10,7 +10,8 @@
 angular.module('floggitPostitsApp')
   .factory('dataStorage', function ($http, $q) {
 
-    var baseUrl = 'http://api.beta2.se/fp-';
+    // var baseUrl = 'http://api.beta2.se/fp-';
+    var baseUrl = 'http://localhost:14782/fp-';
 
     function basicGet(whiteboard, type) {
       var deferred = $q.defer();
@@ -20,13 +21,13 @@ angular.module('floggitPostitsApp')
       });
     }
 
-    function basicPost(whiteboard, type, data) {
-      var deferred = $q.defer();
-      var url = baseUrl + whiteboard + '-' + type;
-      $http.post(url, data).success(function (data) {
-        deferred.resolve(data);
-      });
-    }
+  // function basicPost(whiteboard, type, data) {
+  //   var deferred = $q.defer();
+  //   var url = baseUrl + whiteboard + '-' + type;
+  //   $http.post(url, data).success(function (data) {
+  //     deferred.resolve(data);
+  //   });
+  // }
 
     function getAllCategoriesFor(whiteboard) {
       return basicGet(whiteboard, 'categories');
@@ -37,21 +38,63 @@ angular.module('floggitPostitsApp')
     }
 
 
-    function postDummyData() {
-      for (var i = 0; i < dummyData.categories.length; i = i + 1) {
-        basicPost('testwhiteboard', 'categories', dummyData.categories[i]);
-      }
-      for (var j = 0; j < dummyData.postits.length; j = j + 1) {
-        basicPost('testwhiteboard', 'categories', dummyData.categories[i]);
-      }
-    }
+    // function postDummyData() {
+    //   for (var i = 0; i < dummyData.categories.length; i = i + 1) {
+    //     basicPost('testwhiteboard2', 'categories', dummyData.categories[i]);
+    //   }
+    //   for (var j = 0; j < dummyData.postits.length; j = j + 1) {
+    //     basicPost('testwhiteboard2', 'postits', dummyData.postits[i]);
+    //   }
+    // }
+    // var dummyDataCategories = [{
+    //   'name': 'Todo'
+    // }, {
+    //   'name': 'In progress'
+    // }, {
+    //   'name': 'Done'
+    // }];
 
-    var dummyData = {};
-    dummyData.categories = [];
-    dummyData.postits = [];
-    postDummyData();
+    // var dummyDataPostits = [{
+    //   'title': 'Some good deed.',
+    //   'description': 'I am ver good deed that must be done!',
+    //   'color': 'red',
+    //   'category': 1
+    // }, {
+    //   'title': 'Some bad deed.',
+    //   'description': 'I am ver bad deed that must be done!',
+    //   'color': 'blue',
+    //   'category': 1
+    // }, {
+    //   'title': 'Get angry over shit .',
+    //   'description': 'I very important stuff!',
+    //   'color': 'red',
+    //   'category': 1
+    // }, {
+    //   'title': 'Under progress',
+    //   'description': 'I huiehriueh must be done!',
+    //   'color': 'green',
+    //   'category': 2
+    // }, {
+    //   'title': 'Almost done.',
+    //   'description': 'I am ver bad deed that must be done!',
+    //   'color': 'yellow',
+    //   'category': 2
+    // }, {
+    //   'title': 'Done!!',
+    //   'description': 'I huiehriueh must be done be done!',
+    //   'color': 'green',
+    //   'category': 3
+    // }, {
+    //   'title': 'Done Again',
+    //   'description': 'I anojoijojioj jjj must be done!',
+    //   'color': 'blue',
+    //   'category': 3
+    // }];
 
-
+    // var dummyData = {};
+    // dummyData.categories = dummyDataCategories;
+    // dummyData.postits = dummyDataPostits;
+    // postDummyData();
 
     return {
       getUrl: function () {
--- resources/dummyData.js
+++ resources/dummyData.js
@@ -1,44 +1,44 @@
 var dummyDataCategories = [{
-	"name": "Todo"
+  'name': 'Todo'
 }, {
-	"name": "In progress"
+  'name': 'In progress'
 }, {
-	"name": "Done"
+  'name': 'Done'
 }];
 
 var dummyDataPostits = [{
-	"title": "Some good deed.",
-	"description": "I am ver good deed that must be done!",
-	"color": "red",
-	"category": 1
+  'title': 'Some good deed.',
+  'description': 'I am ver good deed that must be done!',
+  'color': 'red',
+  'category': 1
 }, {
-	"title": "Some bad deed.",
-	"description": "I am ver bad deed that must be done!",
-	"color": "blue",
-	"category": 1
+  'title': 'Some bad deed.',
+  'description': 'I am ver bad deed that must be done!',
+  'color': 'blue',
+  'category': 1
 }, {
-	"title": "Get angry over shit .",
-	"description": "I very important stuff!",
-	"color": "red",
-	"category": 1
+  'title': 'Get angry over shit .',
+  'description': 'I very important stuff!',
+  'color': 'red',
+  'category': 1
 }, {
-	"title": "Under progress",
-	"description": "I huiehriueh must be done!",
-	"color": "green",
-	"category": 2
+  'title': 'Under progress',
+  'description': 'I huiehriueh must be done!',
+  'color': 'green',
+  'category': 2
 }, {
-	"title": "Almost done.",
-	"description": "I am ver bad deed that must be done!",
-	"color": "yellow",
-	"category": 2
+  'title': 'Almost done.',
+  'description': 'I am ver bad deed that must be done!',
+  'color': 'yellow',
+  'category': 2
 }, {
-	"title": "Done!!",
-	"description": "I huiehriueh must be done be done!",
-	"color": "green",
-	"category": 3
+  'title': 'Done!!',
+  'description': 'I huiehriueh must be done be done!',
+  'color': 'green',
+  'category': 3
 }, {
-	"title": "Done Again",
-	"description": "I anojoijojioj jjj must be done!",
-	"color": "blue",
-	"category": 3
-}]
+  'title': 'Done Again',
+  'description': 'I anojoijojioj jjj must be done!',
+  'color': 'blue',
+  'category': 3
+}];
