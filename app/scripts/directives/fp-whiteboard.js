cc76e6f3c12f65d4cc6b8f0359145aa184e0362d
andberg <andreaberglund@hotmail.com>
2014-08-26 14:20:18 +0200

    commiting to get lates version

--- app/scripts/directives/fp-whiteboard.js
+++ app/scripts/directives/fp-whiteboard.js
@@ -16,14 +16,7 @@ angular.module('floggitPostitsApp')
       },
       controller: function ($scope, dataStorage) {
         $scope.url = dataStorage.getUrl();
-        // var categoryPromise = dataStorage.getAllCategoriesFor('testwhiteboard');
-        // categoryPromise.then(function (data) {
-        //   $scope.categories = data;
-        // });
-        // var postitPromise = dataStorage.getAllPostitsFor('testwhiteboard');
-        // postitPromise.then(function (data) {
-        //   $scope.postits = data;
-        // });
+
         var allPromise = dataStorage.getAll($scope.name);
         allPromise.then(function (data) {
           $scope.whiteboard = data;
