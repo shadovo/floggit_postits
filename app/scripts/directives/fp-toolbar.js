2c9cacddc392a5a3d0176e38f5d43b3ed49d0d4b
andberg <andreaberglund@hotmail.com>
2014-08-27 10:10:41 +0200

    started on whiteboard and design for post-its

--- app/scripts/directives/fp-toolbar.js
+++ app/scripts/directives/fp-toolbar.js
@@ -7,12 +7,12 @@
  * # fpToolbar
  */
 angular.module('floggitPostitsApp')
-  .directive('fpToolbar', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element) {
-        element.text('this is the fpToolbar directive');
-      }
-    };
-  });
+	.directive('fpToolbar', function () {
+		return {
+			templateUrl: 'views/toolbar.html',
+			restrict: 'E',
+			link: function postLink(scope, element) {
+				element.text('this is the fpToolbar directive');
+			}
+		};
+	});
