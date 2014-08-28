683950592a14f6ffc09b1f5e1ea54c9869c7d8e7
andberg <andreaberglund@hotmail.com>
2014-08-28 10:37:30 +0200

    Addes CSS for the four different colors, and the functionality för update postits and categories. I also added reload functionality for when a postit has updated a category and or have been deleted

--- app/scripts/directives/fp-postit.js
+++ app/scripts/directives/fp-postit.js
@@ -14,52 +14,53 @@ angular.module('floggitPostitsApp')
       scope: {
         postit: '='
       },
-      controller: function ($scope, dataStorage) {
+      controller: function ($scope, $route, dataStorage) {
 
         $scope.categories = [];
         $scope.colors = ['red', 'blue', 'green', 'yellow'];
 
-        var promise = dataStorage.getAllCategoriesFor('testwhiteboard');
-        promise.then(function (data) {
-          $scope.categories = data;
-          for (var i = 0; i < $scope.categories.length; i++) {
-            if ($scope.categories[i].id === $scope.postit.category) {
-              $scope.newCategory = $scope.categories[i];
+        var getAllDataFromDataStorage = function () {
+          dataStorage.getAllCategoriesFor('testwhiteboard').then(function (data) {
+            $scope.categories = data;
+            for (var i = 0; i < $scope.categories.length; i++) {
+              if ($scope.categories[i].id === $scope.postit.category) {
+                $scope.newCategory = $scope.categories[i];
+              }
             }
-          }
-        });
-
-        $scope.$watch('newCategory', function () {
-          $scope.updatePostitCategory();
-        });
+          });
+        };
+        getAllDataFromDataStorage();
 
         var title = $scope.postit.title;
         var description = $scope.postit.description;
         var category = $scope.postit.category;
         var color = $scope.postit.color;
-
         $scope.newColor = color;
 
-        $scope.$watch('newColor', function () {
-          $scope.updatePostitColor();
+        $scope.$watch('newCategory', function () {
+          updatePostitCategory();
         });
 
+        $scope.$watch('newColor', function () {
+          updatePostitColor();
+        });
 
-        $scope.updatePostitColor = function () {
-          console.log($scope.color);
+        var updatePostitColor = function () {
           if ($scope.newColor !== undefined && $scope.newColor !== color) {
             $scope.postit.color = $scope.newColor;
             dataStorage.updatePostit('testwhiteboard', $scope.postit);
-            color = $scope.postit.color;
-            console.log(color);
           }
         };
 
-        $scope.updatePostitCategory = function () {
+        var updatePostitCategory = function () {
           if ($scope.newCategory !== undefined && $scope.newCategory.id !== category) {
             $scope.postit.category = $scope.newCategory.id;
-            dataStorage.updatePostit('testwhiteboard', $scope.postit);
-            category = $scope.postit.category;
+            dataStorage.updatePostit('testwhiteboard', $scope.postit).then(function () {
+              category = $scope.postit.category;
+              getAllDataFromDataStorage();
+            }).then(function () {
+              $route.reload();
+            });
           }
         };
 
@@ -74,7 +75,11 @@ angular.module('floggitPostitsApp')
           var answer = confirm('Are you sure that you want to delete this postit?');
           if (answer === true) {
             console.log('ID: ' + $scope.postit.id);
-            dataStorage.deletePostit('testwhiteboard', $scope.postit.id);
+            dataStorage.deletePostit('testwhiteboard', $scope.postit.id).then(function () {
+              getAllDataFromDataStorage();
+            }).then(function () {
+              $route.reload();
+            });
           }
         };
       }
